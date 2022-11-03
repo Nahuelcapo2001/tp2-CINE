e20ea1e23c26b0c31c6bf9a6d935dab96d36b3d4
graciela1990 <gomez.ge@hotmail.com>
2022-11-03 09:50:15 -0300

    MODIFICAR SALAS

--- tp2-CINE ACTUALIZADO v4/tp cine labo 2/modificarSalas.h
+++ tp2-CINE ACTUALIZADO v4/tp cine labo 2/modificarSalas.h
@@ -2,6 +2,9 @@
 #define MODIFICARSALAS_H_INCLUDED
 
 void modificarSalas();
+bool modificarTipoSala();
+bool modificarCantButacas();
+bool modificarPrecioSala();
 
 void modificarSalas()
 {
@@ -21,22 +24,102 @@ void modificarSalas()
     {
         switch(opc)
         {
-        case 1:
+        case 1: if( modificarTipoSala()==true)
+            {
+                cout<<"EL TIPO DE SALA HA SIDO MODIFICADO CORRECTAMENTE"<<endl;
+                return;
+            }
+            else
+            {
+                cout<<"EL ID DE SALA NO ES VALIDO"<<endl;
+            }
             break;
-        case 2:
+        case 2:if( modificarCantButacas()==true)
+            {
+                cout<<"LA CANTIDAD DE BUTACAS HA SIDO MODIFICADA CORRECTAMENTE"<<endl;
+                return;
+            }
+            else
+            {
+                cout<<"EL ID DE SALA NO ES VALIDO"<<endl;
+            }
             break;
-        case 3:
+        case 3:if( modificarPrecioSala()==true)
+            {
+                cout<<"EL PRECIO DE SALA HA SIDO MODIFICADO CORRECTAMENTE"<<endl;
+                return;
+            }
+            else
+            {
+                cout<<"EL ID DE SALA NO ES VALIDO"<<endl;
+            }
             break;
-        case 0:
+        case 0:return;
             break;
-        default:
+        default:cout<<"ELIJA UNA OPCION VALIDA"<<endl;
             break;
-
-
         }
     }
-
 }
+bool modificarTipoSala()
+{
+   Salas obj;
+    int pos=0,id;
+    char tipo[3];
+    cout<<"INGRESE EL NUMERO DE ID DE LA SALA A MODIFICAR: ";
+    cin>>id;
+    while(obj.leerEnDisco(pos++))
+    {
+        if(id==obj.getidSala()&& obj.getEstado()==true)
+        {
+            cout<<"INGRESE LA TIPO DE SALA: ";
+            cargarCadena(tipo,2);
+            obj.setTipo(tipo);
+            obj.ModificarEnDisco(pos-1);
+            return true;
+        }
+    }
+    return false;
+}
+bool modificarCantButacas()
+{
+Salas obj;
+    int pos=0,id,cant;
+    cout<<"INGRESE EL NUMERO DE ID DE LA SALA A MODIFICAR: ";
+    cin>>id;
+    while(obj.leerEnDisco(pos++))
+    {
+        if(id==obj.getidSala()&& obj.getEstado()==true)
+        {
+            cout<<"INGRESE LA CANTIDAD DE BUTACAS: ";
+            cin>>cant;
+            obj.setPlazas(cant);
+            obj.ModificarEnDisco(pos-1);
+            return true;
+        }
+    }
+    return false;
+}
+bool modificarPrecioSala()
+{
+Salas obj;
+    int pos=0,id,prec;
+    cout<<"INGRESE EL NUMERO DE ID DE LA SALA A MODIFICAR: ";
+    cin>>id;
+    while(obj.leerEnDisco(pos++))
+    {
+        if(id==obj.getidSala()&& obj.getEstado()==true)
+        {
+            cout<<"INGRESE EL PRECIO DE LA SALA: ";
+            cin>>prec;
+            obj.setPrecio(prec);
+            obj.ModificarEnDisco(pos-1);
+            return true;
+        }
+    }
+    return false;
+}
+
 
 
 #endif // MODIFICARSALAS_H_INCLUDED
